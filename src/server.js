36a3904bb27700916887ec95a0d1c6fd3b06d055
Victor Barros <victorclovisb@hotmail.com>
2022-08-27 10:48:05 -0300

    Preparing to deploy

--- src/server.js
+++ src/server.js
@@ -1,4 +1,6 @@
 require('express-async-errors')
+require('dotenv/config')
+
 const express = require('express')
 const cors = require('cors')
 
@@ -33,5 +35,5 @@ app.use((error, req, res, next) => {
   })
 })
 
-const PORT = 3333
+const PORT = process.env.PORT || 3333
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}`))
